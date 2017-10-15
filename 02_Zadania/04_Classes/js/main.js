8c7a06f54a4a881a6567136904715231fbcfaa77
pv-dv <pavv.dev@gmail.com>
2017-10-15 09:01:29 +0200

    v2
    - added classes
    - added bubble sort

--- 02_Zadania/04_Classes/js/main.js
+++ 02_Zadania/04_Classes/js/main.js
@@ -5,11 +5,11 @@ class Book {
         this.isRead = isRead;
     }
     
-    describeBook (){
+    describeBook (){  
+        var answer = (this.isRead == false)? "No":"Yes";  
         
-        var description = ("Book has title: " + this.title +
-                "\nWritten by: " + this.author +
-                "\nHas been read? " +this.isRead);
+        var description = ("Book title is: " + this.title + " & was written by: " + this.author + ". Was it read? " + answer);
+    
         return description;
     }
 }
@@ -18,17 +18,25 @@ var witcher = new Book("Witcher", "Andrzej Sapkowski", true);
 var borne = new Book("Borne Ultimatum", "Robert Ludlum", false);
 var requirem = new Book("Requiem for American Dream", "Noam Chomsky", false);
 
-
 var booksArray = [witcher, borne, requirem];
 
-console.log(booksArray);
-
 function noOfRead(booksArray) {
-    var noOfReadBooks;
+    var noOfReadBooks = 0;
     
-    booksArray.forEach(function(element, index){
-        console.log(booksArray.element.title);
-        
-    });
+    for (var i=0; i < booksArray.length; i++){
+        if (booksArray[i].isRead == true) {
+            noOfReadBooks += 1;
+        }
+        var bookDesc = booksArray[i].describeBook();
+        console.log(bookDesc);
+    }
     
+    return noOfReadBooks;
 }
+
+var noOfReadBooks = noOfRead(booksArray);
+var has = "has";
+if (noOfReadBooks > 1){
+    has = "have";
+}
+console.log(noOfReadBooks + " out of " + booksArray.length +" books "+ has + " been read.")
---
+++ 02_Zadania/05_Bubble sorting/js/main.js
@@ -0,0 +1,23 @@
+function bubble(array){
+    var swapsOccured;
+    do{
+        swapsOccured = 0;
+        for(var i = 0; i < array.length; i++){
+            var nextIndex = i + 1;
+            if(array[i] > array[nextIndex]){
+                
+                swapsOccured += 1;
+                var temp = array[i];
+                array[i] = array[nextIndex];
+                array[nextIndex] = temp;
+            }
+        }         
+    } while (swapsOccured !== 0);
+}
+
+var array = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78,
+34, 1e2, 45, 67, 98, 1];
+console.log("Unsorted: " + array);
+
+bubble(array);
+console.log("Sorted: " + array);
